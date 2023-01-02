e7f4559f32725ddb5bbd619fb0adfa585166a632
Barathraj12345 <120628438+Barathraj12345@users.noreply.github.com>
2023-01-02 11:17:41 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,73 @@
+var resume={
+    "career objective":{
+     'career':"to be part of company that indulges professional growth which provides challenging and rewarding career whilr allowing me to uitliz my knowlege and skills"
+    },
+  "personal details":
+  {
+    'name':"T.BarathRaj",
+    'father name': "R.Thiruvenkadam",
+    'mother name':"T.Gowri",
+    'date of birth': "05-07-2001",
+    'gender':"male",
+    'marital status':"single",
+    'nationality':"indian",
+    'religion':"hindu",
+    'mobile no': 7824804291
+  }
+}
+console.log(resume);
+console.table(resume["personal details"]);
+console.assert(resume);
+console.log(resume);
+var head1=[ 
+{
+    
+       'careerobjective' : "to be a part o fo company that indulges professional growth which provides challenging and rewaarding career whilw allowing me to utilize my knowledge and skills.",
+},
+{
+'summary' :"i am good fun and frendly person, i am honest and punctual. i ahve good communication skill. i have creative mind and am always realy for new challenges. i can deal with difficulti situations. maybe a lot of people are more the good then me, but with perseverance and strong will can defeat all."
+},
+{
+    'personalskills':"vreativity, interpersonal skills, critical thinking, problem solving, public speaking, customer service skills, teamwork skill, communication."
+}
+]
+ var second =[
+{
+    'Qualification':'BCA',
+    'Schoolcollege':'VEL TECH ARTS college',
+    'Board':'University of madras',
+    'yearofpassing':'2022',
+    'percentageCGPA':'85%'
+},
+{
+    'Qualification':'HSC',
+    'Schoolcollege':'sacred heart school',
+    'Board':'State board',
+    'yearofpassing':'2019',
+    'percentageCGPA':'70%'
+},
+{
+    'Qualification':'SSLC',
+    'Schoolcollege':'sacred heart school',
+    'Board':'State board',
+    'yearofpassing':'2017',
+    'percentageCGPA':'87%'
+}
+ ]
+
+ var skills=
+    ["c","c++","HTML5", "CSS3", "JAVASCRIPT", "RDBMS,", "SQL", "ms office"]
+    var personalinfo={
+'name':"T.BarathRaj",
+'father':"R.Thiruvenkadam",
+'DOB':"05-07-2001",
+'Gender':"male",
+'Maritalstatus':"single",
+'Nationality':"indian",
+'Religion':"HIndu",
+'Mobileno': 7824804291
+    }
+console.table(head1);
+console.table(second)
+console.log(skills)
+console.table(personalinfo)
